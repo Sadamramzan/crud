8ea1d3554f9b4fcf2eb6534eedfb3b2f1c0e6c59
Sadamramzan <135207789+Sadamramzan@users.noreply.github.com>
2023-07-20 11:22:14 +0500

    crud

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 // import "./App.css";
 import CrudCard from "./Components/Crud/CrudCard";
-// import Todo from "./Components/Todo/Todo";
 
 function App() {
   return (
     <div className="App">
       <CrudCard />
-      {/* <Todo /> */}
     </div>
   );
 }
